78547866f8fe7f9aeb3703d46dd6febd78ab3992
Alok Prasad <alokprasad@CREDBLRMAC1418.local>
2023-02-14 21:45:32 +0530

    updated api

--- demo-app/app.js
+++ demo-app/app.js
@@ -1,16 +1,31 @@
-//Require module
 const express = require('express');
+const cors = require('cors')
+
 // Express Initialize
 const app = express();
-
-
+const fetch = (...args) =>
+    import ('node-fetch').then(({ default: fetch }) => fetch(...args));
 const port = 8000;
-app.listen(port,()=> {
-console.log('listen port 8000');
-})
+app.use(express.json());
 
+/* CORS configuration */
+app.use(cors({
+    origin: "*",
+}));
 
 //create api
-app.get('/hello_world', (req,res)=>{
-    res.send('Hello World from...<Need to add instance id>');
-    })
+app.get("/", async(req, res) => {
+    const clientip = req.ip;
+    const timestamp = new Date();
+    const response = await fetch('http://169.254.169.254/latest/meta-data/instance-id');
+    const instanceid = await response.text();
+    console.log("Sent hello-world to " + clientip + " at " + timestamp + " from " + instanceid)
+    res.json({
+        instance_id: instanceid,
+        message: "hello world from " + instanceid + " to client " + clientip,
+    });
+});
+
+app.listen(port, () => {
+    console.log('listen port 8000');
+})
